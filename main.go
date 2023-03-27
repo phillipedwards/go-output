6f7656e5eb74a8ba8843955ea15596fe217aaee6
phillip edwards <phillipedwards02@gmail.com>
2023-03-27 11:17:56 -0700

    remove aws

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/phillipedwards/pulumi-notes"
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
 	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -10,16 +9,10 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
-		if err != nil {
-			return err
-		}
 
 		_ = sum.GetSum(1, 2)
 
-		out := pulumi.JSONMarshal(bucket.Arn)
-
-		_, err = random.NewRandomId(ctx, "rrr", &random.RandomIdArgs{
+		_, err := random.NewRandomId(ctx, "rrr", &random.RandomIdArgs{
 			ByteLength: pulumi.Int(4),
 		})
 
@@ -28,8 +21,6 @@ func main() {
 		}
 
 		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
-		ctx.Export("json-out", out)
 		return nil
 	})
 }
